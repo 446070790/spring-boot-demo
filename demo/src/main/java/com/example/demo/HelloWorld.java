73bfea1644170fc40be0edc275a1b43a75e47cb2
446070790 <Gavin520Helen@163.com>
2020-05-22 23:01:48 +0800

    IHD-123 try

--- demo/src/main/java/com/example/demo/HelloWorld.java
+++ demo/src/main/java/com/example/demo/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("试试git");
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        System.out.println("next = :" + next);
     }
 }
